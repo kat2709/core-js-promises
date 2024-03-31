1e1d7aebe1c9f47e36f05dd334eecbe8d0165c36
Katsiaryna Rubchanka <6923845@mail.ru>
2024-03-31 14:00:44 +0300

    feat: add solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -55,8 +55,21 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const obj = { count: 0 };
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i += 1) {
+      const p = promises[i];
+      p.then((val) => {
+        resolve(val);
+      }).catch(() => {
+        obj.count += 1;
+        if (obj.count === promises.length) {
+          reject();
+        }
+      });
+    }
+  });
 }
 
 /**
